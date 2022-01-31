2a73596b67fa639b56280ef6edbba8933680b136
malcolmEZE <malcolm75018@hotmail.fr>
2022-01-31 06:47:00 -0500

    new commit

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World I am Malcolm EZE I am 26 yrs old!") ;
+        System.out.println("Hello, World I am Malcolm EZE I am 26!") ;
     }
 }
 
